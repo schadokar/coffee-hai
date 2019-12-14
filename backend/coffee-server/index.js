6b50b6a083d16e1f231bf8e44064fc481d26dd86
schadokar <shubhamchadokar04@gmail.com>
2019-12-14 16:44:33 +0530

    cs: single sendOTP and verifyOTP function

--- backend/coffee-server/index.js
+++ backend/coffee-server/index.js
@@ -1,13 +1,21 @@
 require("dotenv");
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
 const authenticateAPI = require("./routes/authenticate-api.js");
 
 // Express body parser
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: false,
+    parameterLimit: 50000
+  })
+);
 
 // use the routes specified in route folder
 app.use("/api/v1", authenticateAPI);
